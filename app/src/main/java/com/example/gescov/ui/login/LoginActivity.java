d76a41fd3f663da65eba08c10e146158a5966345
Ponc <ponpcap@hotmail.com>
2020-10-21 21:49:20 +0200

    first part of the merge with the broken navigator

--- app/src/main/java/com/example/gescov/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/gescov/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -23,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gescov.R;
+import com.example.gescov.navigation.NavigationMenu;
 import com.example.gescov.ui.login.LoginViewModel;
 import com.example.gescov.ui.login.LoginViewModelFactory;
 
@@ -122,7 +124,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
+        Intent intent = new Intent(this, NavigationMenu.class);
+        startActivity(intent);
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
