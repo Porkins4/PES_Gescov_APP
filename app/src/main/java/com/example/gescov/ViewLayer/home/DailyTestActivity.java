eb8ec4de4c533086f52155f032fa4f2740b27eae
ANASinfad <slardar45lp>
2020-11-06 23:48:55 +0100

    Activity

---
+++ app/src/main/java/com/example/gescov/ViewLayer/home/DailyTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.gescov.ViewLayer.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gescov.R;
+
+public class DailyTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_test);
+    }
+}
