66b4b152442a448d6d2a493c5e84ba3fb6f66cf8
immunoz <isaac-mc99@hotmail.com>
2020-11-12 23:46:17 +0100

    main activity arreglada

--- app/src/main/java/com/example/gescov/ViewLayer/MainView/MainViewActivity.java
+++ app/src/main/java/com/example/gescov/ViewLayer/MainView/MainViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.gescov.ViewLayer.MainView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,15 +14,22 @@ import com.example.gescov.ViewLayer.SignUpIn.LoginActivity;
 import com.example.gescov.ViewLayer.navigation.NavigationMenu;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainViewActivity extends AppCompatActivity {
+
+    private GoogleSignInClient googleSignInClient;
 
     private GoogleSignInAccount account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
-        CountDownTimer timer = new CountDownTimer(1500,1000) {
+        /*CountDownTimer timer = new CountDownTimer(1500,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
@@ -29,9 +37,38 @@ public class MainViewActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                checkUserLoggedIn();
+
             }
-        }.start();
+        }.start();*/
+        initGoogleClient();
+    }
+
+    private void initGoogleClient() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+        googleSignInClient.silentSignIn()
+                .addOnCompleteListener(
+                        this,
+                        new OnCompleteListener<GoogleSignInAccount>() {
+                            @Override
+                            public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                handleSignInResult(task);
+                            }
+                        });
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            LoggedInUser.setCurrentLoggedUser(account);
+            succesfullLoginUI();
+            // TODO(developer): send ID Token to server and validate
+        } catch (ApiException e) {
+            checkUserLoggedIn();
+        }
     }
 
     private void startLoginActivity() {
