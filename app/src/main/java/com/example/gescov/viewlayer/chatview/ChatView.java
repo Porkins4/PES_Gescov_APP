d6e84626187a648908a748382fc5190eacf68ff2
immunoz <isaac.marcelo.munoz@est.fib.upc.edu>
2020-12-02 18:52:55 +0100

    layout chat done

---
+++ app/src/main/java/com/example/gescov/viewlayer/chatview/ChatView.java
@@ -0,0 +1,16 @@
+package com.example.gescov.viewlayer.chatview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gescov.R;
+
+public class ChatView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_view);
+    }
+}
