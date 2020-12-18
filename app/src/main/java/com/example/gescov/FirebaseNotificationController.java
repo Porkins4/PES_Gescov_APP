853a9ad75fc24394a492f26a565d4e7f8a600047
immunoz <isaac.marcelo.munoz@est.fib.upc.edu>
2020-12-18 01:25:56 +0100

    bien configurado

---
+++ app/src/main/java/com/example/gescov/FirebaseNotificationController.java
@@ -0,0 +1,9 @@
+package com.example.gescov;
+
+import com.example.gescov.viewlayer.Singletons.GescovApplication;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FirebaseNotificationController extends FirebaseMessagingService {
+
+}
