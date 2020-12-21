39bd7e9b274eb489dbbd4b4ea6357f237dce10f9
Ponc <ponpcap@hotmail.com>
2020-12-21 21:14:45 +0100

    removed create button from SchoolUsersListActivity

--- app/src/main/java/com/example/gescov/viewlayer/Templates/GescovModelListedActivity.java
+++ app/src/main/java/com/example/gescov/viewlayer/Templates/GescovModelListedActivity.java
@@ -1,20 +1,24 @@
 package com.example.gescov.viewlayer.Templates;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.gescov.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public abstract class GescovModelListedActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_school_classrom_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.fab);
         setSupportActionBar(toolbar);
 
         ListView list = findViewById(R.id.classrooms_list);
@@ -28,4 +32,7 @@ public abstract class GescovModelListedActivity extends AppCompatActivity {
     }
 
     protected abstract GescovModelListedController setModelController();
+    protected void setFABVisible(boolean visible) {
+        fab.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
 }
